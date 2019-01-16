e3bd19574e5cd5e044a9434da72ce54ffa965026
Luiz Fernando de Souza Filho <fernandoperigolo@gmail.com>
2019-01-16 16:15:22 -0200

    Building static view of post on list

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -5,8 +5,18 @@ class PostListItem extends Component {
   render() {
     const { post } = this.props
     return (
-      <div>
-        Title: {post.title}
+      <div className='post-list-item'>
+        <h2>{post.title}</h2>
+        <p className='post-list-item-info'>
+          <span className='post-list-item-author'>By: {post.author}</span>
+          <span className='post-list-item-datetime'>Date and Time: {post.timestamp}</span>
+          <span className='post-list-item-comment-count'>Comments: {post.commentCount}</span>
+        </p>
+        <div className='post-list-item-votescore'>
+          <a href='#' className='post-list-item-vote post-list-item-vote-up'>Vote Up</a>
+          <span className='post-list-item-score'>{post.voteScore}</span>
+          <a href='#' className='post-list-item-vote post-list-item-vote-down'>Vote Down</a>
+        </div>
       </div>
     )
   }
