86bcef243d2d86069823a1def07983d5ed0921e0
Luiz Fernando de Souza Filho <fernandoperigolo@gmail.com>
2019-01-17 15:23:01 -0200

    Fixing error where posts are not loaded on switch between categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,8 +6,24 @@ import { connect } from 'react-redux'
 import { handleCategoryData } from '../actions/views'
 
 class Category extends Component {
+  state = {
+    currentCategory: '',
+  }
+
   componentDidMount() {
+    this.setState(() => ({
+      currentCategory: this.props.match.params.categoryPath
+    }))
     this.props.dispatch(handleCategoryData(this.props.match.params.categoryPath))
+  }
+
+  componentDidUpdate() {
+    if (this.state.currentCategory !== this.props.match.params.categoryPath) {
+      this.setState(() => ({
+        currentCategory: this.props.match.params.categoryPath
+      }))
+      this.props.dispatch(handleCategoryData(this.props.match.params.categoryPath))
+    }
   }
 
   render() {
@@ -18,7 +34,10 @@ class Category extends Component {
         <div className='wrap-content'>
           <div className='content-container'>
             <h3>Category - {this.props.match.params.categoryPath}</h3>
-            <PostList postsIds={this.props.postsIds} />
+            {this.props.postsIds.length
+              ? <PostList postsIds={this.props.postsIds} />
+              : <p>No posts here...</p>
+            }
           </div>
         </div>
 
